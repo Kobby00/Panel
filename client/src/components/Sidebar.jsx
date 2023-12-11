4ca632b82a94bfde8259023ec83bd075f7a33e53
Kobby00 <kwabenaoware243@gmail.com>
2023-12-11 13:26:44 +0000

    update 3

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -17,9 +17,7 @@ import {
   ChevronLeft,
   ChevronRightOutlined,
   HomeOutlined,
-  HailOutlined,
   ShoppingCartOutlined,
-  LocalLibraryOutlined,
   Groups2Outlined,
   ReceiptLongOutlined,
   PublicOutlined,
@@ -45,16 +43,16 @@ const navItems = [
     icon: null,
   },
   {
-    text: "Driver",
+    text: "Products",
+    icon: <ShoppingCartOutlined />,
+  },
+  {
+    text: "Customers",
     icon: <Groups2Outlined />,
   },
   {
-    text: "Customer",
-    icon: <HailOutlined />,
-  },
-  {
-    text: "Manual Booking",
-    icon: < LocalLibraryOutlined/>,
+    text: "Transactions",
+    icon: <ReceiptLongOutlined />,
   },
   {
     text: "Geography",
@@ -92,6 +90,7 @@ const navItems = [
     text: "Performance",
     icon: <TrendingUpOutlined />,
   },
+  
 ];
 
 const Sidebar = ({
@@ -128,13 +127,13 @@ const Sidebar = ({
               width: drawerWidth,
             },
           }}
-            >
-          <Box width="100%">
-            <Box m="1.5rem 2rem 2rem 3rem">
+        >
+          <Box width ="100%">
+            <Box m ="1.5rem 2rem 2rem 3rem">
               <FlexBetween color={theme.palette.secondary.main}>
                 <Box display="flex" alignItems="center" gap="0.5rem">
                   <Typography variant="h4" fontWeight="bold">
-                    FLEET
+                    ECOMVISION
                   </Typography>
                 </Box>
                 {!isNonMobile && (
@@ -195,7 +194,41 @@ const Sidebar = ({
             </List>
           </Box>
 
-          
+          <Box>
+            <Divider />
+            <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+              <Box
+                component="img"
+                alt="profile"
+                src={profileImage}
+                height="40px"
+                width="40px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.9rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.8rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <SettingsOutlined
+                sx={{
+                  color: theme.palette.secondary[300],
+                  fontSize: "25px ",
+                }}
+              />
+            </FlexBetween>
+          </Box>
         </Drawer>
       )}
     </Box>
--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -7,16 +7,15 @@ import Sidebar from "components/Sidebar";
 import { useGetUserQuery } from "state/api";
 
 const Layout = () => {
-const isNonMobile = useMediaQuery("(min-width: 600px)");
-const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-const userId = useSelector((state) => state.global.userId);
-const { data } = useGetUserQuery(userId);
-console.log(data)
-
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+  const { data } = useGetUserQuery(userId);
 
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
       <Sidebar
+        user={data || {}}
         isNonMobile={isNonMobile}
         drawerWidth="250px"
         isSidebarOpen={isSidebarOpen}
@@ -24,6 +23,7 @@ console.log(data)
       />
       <Box flexGrow={1}>
         <Navbar
+          user={data || {}}
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
         />
